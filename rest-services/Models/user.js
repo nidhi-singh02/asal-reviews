f168ca104f7ab2930907a76652391dbc3f9ad24d
Saurabh Singh <saurabhsingh121@gmail.com>
2022-04-27 00:42:14 +0530

    signin, signup and signout routes

--- rest-services/Models/user.js
+++ rest-services/Models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { Password } = require("../Services/password");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -11,6 +12,9 @@ const UserSchema = new mongoose.Schema(
     Email: {
       type: String,
     },
+    Hashed_Password: {
+      type: String,
+    },
     PhoneNo: {
       type: Number,
     },
@@ -20,7 +24,26 @@ const UserSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret.UserId;
+        delete ret.UserId;
+        delete ret._id;
+        delete ret.Hashed_Password;
+        delete ret.__v;
+      },
+    },
   }
 );
+
+UserSchema.pre("save", async function (done) {
+  if (this.isModified("Hashed_Password")) {
+    const hashed = await Password.toHash(this.get("Hashed_Password"));
+    this.set("Hashed_Password", hashed);
+  }
+  done();
+});
 
 module.exports = mongoose.model("users", UserSchema);
--- rest-services/Routers/route.js
+++ rest-services/Routers/route.js
@@ -5,7 +5,10 @@ const express = require("express"),
 
 route.post("/user/createReview", reviewController.createReview);
 route.post("/user/getReview", reviewController.getReviews);
-route.get("user/upvote/:id", reviewController.upvote);
+route.get("/user/upvote/:id", reviewController.upvote);
+route.post("/user/signup", reviewController.signup);
+route.post("/user/signin", userController.signin);
+route.post("/user/singout", userController.signout);
 route.get("/user/getUser/:id", userController.getUser);
 
 module.exports = route;
