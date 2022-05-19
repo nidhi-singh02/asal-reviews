452e8e427b2594737190e758ea8356e8a03c3c1e
joltsama <tarunkumarsingh98@gmail.com>
2022-05-19 20:35:46 +0530

    change api endpoint

--- asal-reviews-ui/src/environments/environment.ts
+++ asal-reviews-ui/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://asal-review-backend.herokuapp.com'
+  // apiUrl: 'https://asal-review-backend.herokuapp.com'
+  apiUrl: "http://10.160.217.8:5000",
 };
